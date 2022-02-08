ee7fde251cf4ed973c4b2766a8ccee2e34e33957
Jamie Thomson <thomson.j@etoncollege.org.uk>
2022-02-08 11:18:55 +0000

    map

--- CommentCardGenerator/ContentView.swift
+++ CommentCardGenerator/ContentView.swift
@@ -8,9 +8,20 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var feelings = ""
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack{
+            Text("are you?")
+                .padding()
+            Button("happy", action: {feelings = "happy"})
+                .padding()
+            Button("struggling", action: {feelings = "struggling"})
+                .padding()
+            Button("content", action: {feelings = "content"})
+                .padding()
+            Text("I am \(feelings) with this subject at the moment")
+        }
+
     }
 }
 
