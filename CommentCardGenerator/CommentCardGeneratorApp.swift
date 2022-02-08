eb6cbe7a8872cd644287b4ea9ace6904a0204d01
Jamie Thomson <thomson.j@etoncollege.org.uk>
2022-02-08 10:52:07 +0000

    Initial Commit

---
+++ CommentCardGenerator/CommentCardGeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CommentCardGeneratorApp.swift
+//  CommentCardGenerator
+//
+//  Created by Thomson, Jamie (NA) on 08/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct CommentCardGeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
